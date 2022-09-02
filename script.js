820a4b30c6ca0fbb52062696459c68fd41bdf847
Ahindra D <official.ahindra@gmail.com>
2022-09-02 09:51:34 +0530

    done

---
+++ script.js
@@ -0,0 +1,32 @@
+targetBottom = document.querySelector(".bottom");
+targetInput = document.querySelector(".input-name")
+
+function addPeople() {
+    let name = targetInput.value;
+    targetInput.value = "";
+    let newElem = document.createElement('div');
+    newElem.setAttribute('class', 'float');
+
+    //IMAGE
+    let newAvatar=document.createElement("img");
+    newAvatar.src ="./images/avatar.png";
+
+    //NAME
+    let newName = document.createElement('p');
+    newName.innerHTML = name;
+    newName.setAttribute('class', 'name');
+
+    //REMOVE
+    let newDel = document.createElement('button');
+    newDel.innerHTML = 'x';
+    newDel.setAttribute('class', "del");
+    newDel.addEventListener("click", function handleClick(event) {
+        targetBottom.removeChild(event.target.parentElement);
+    });
+
+
+    newElem.appendChild(newAvatar);
+    newElem.appendChild(newName);
+    newElem.appendChild(newDel);
+    targetBottom.appendChild(newElem);
+}
